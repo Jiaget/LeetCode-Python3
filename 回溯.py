6deaa4316ac1a4d19991ea9e6bb93cb256d073a8
ASUS <359453987@qq.com>
2021-02-06 19:34:43 +0800

    1

--- 回溯.py
+++ 回溯.py
@@ -1,62 +1,106 @@
 from typing import List
+
 # 回溯：
 # 回溯的递归过程中会出现多个支线，在这些支线中会存在成功与失败，回溯要在出现失败的时候回溯到上一个状态。
 if __name__ == '__main__':
-    def solveSudoku(board: List[List[str]]) -> None:
-        # 使用集合，方便移除已用数字。list的减法只有set()有效
-        nums = {'1', '2', '3', '4', '5', '6', '7', '8', '9'}
-        rows = [set() for _ in range(9)]
-        cols = [set() for _ in range(9)]
-        blocks = [set() for _ in range(9)]
-        blanks = []
-        for i in range(9):
-            for j in range(9):
-                value = board[i][j]
-                bIndex = i // 3 * 3 + j // 3
-                if board[i][j] == '.':
-                    blanks.append((i, j))
-                else:
-                    rows[i].add(value)
-                    cols[j].add(value)
-                    blocks[bIndex].add(value)
 
-        # 将blanks一个个使用回溯法填入满足当前条件的数。
-        def backTrace(n):
-            # 遍历结束，此支线success。
-            if n == len(blanks):
-                return True
-            # 获取空缺的坐标
-            i, j = blanks[n]
-            bIndex = i // 3 * 3 + j // 3
-            # 获取(i, j)可以填入的数字rest
-            rest = nums - rows[i] - cols[j] - blocks[bIndex]
-            # rest为空时，表示当前空缺没办法填入数组，递归失败。
-            if not rest:
-                return False
-            # 逐个放入可填数字，并递归判断此支线的可行性。
-            for number in rest:
-                board[i][j] = number
-                rows[i].add(number)
-                cols[j].add(number)
-                blocks[bIndex].add(number)
-                # 递归进行验证下一可填数字。
-                if backTrace(n + 1):
-                    return True
-                rows[i].remove(number)
-                cols[j].remove(number)
-                blocks[bIndex].remove(number)
-        backTrace(0)
+    def combinationSum2(candidates: List[int], target: int) -> List[List[int]]:
+        res = newRes = []
 
-    board = [
-        ["5", "3", ".", ".", "7", ".", ".", ".", "."],
-        ["6", ".", ".", "1", "9", "5", ".", ".", "."],
-        [".", "9", "8", ".", ".", ".", ".", "6", "."],
-        ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
-        ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
-        ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
-        [".", "6", ".", ".", ".", ".", "2", "8", "."],
-        [".", ".", ".", "4", "1", "9", ".", ".", "5"],
-        [".", ".", ".", ".", "8", ".", ".", "7", "9"]
-    ]
-    solveSudoku(board)
-    print(board)
+        def dfs(candidates: List[int], index, remain, value):
+            n = len(candidates)
+            if remain < 0:
+                return
+            if remain == 0:
+                res.append(sorted(value))
+                return
+            for i in range(index + 1, n):
+                dfs(candidates, i, remain - candidates[i], value + [candidates[i]])
+        dfs(candidates, 0, target, [])
+        return list(set([tuple(t) for t in res]))
+
+
+    candidates = [1]
+    target = 1
+    print(combinationSum2(candidates, target))
+
+    def combinationSum(candidates: List[int], target: int) -> List[List[int]]:
+        res = []
+
+        def dfs(candidates, index, remain, value):
+            n = len(candidates)
+            if remain < 0:
+                return
+            if remain == 0:
+                res.append(value)
+                return
+            for i in range(index, n):
+                if remain < 0:
+                    break
+                dfs(candidates, i, remain - candidates[i], value + [candidates[i]])
+
+        dfs(candidates, 0, target, [])
+        return res
+
+
+    candidates = [1]
+    target = 1
+    print(combinationSum(candidates, target))
+    # def solveSudoku(board: List[List[str]]) -> None:
+    #     # 使用集合，方便移除已用数字。list的减法只有set()有效
+    #     nums = {'1', '2', '3', '4', '5', '6', '7', '8', '9'}
+    #     rows = [set() for _ in range(9)]
+    #     cols = [set() for _ in range(9)]
+    #     blocks = [set() for _ in range(9)]
+    #     blanks = []
+    #     for i in range(9):
+    #         for j in range(9):
+    #             value = board[i][j]
+    #             bIndex = i // 3 * 3 + j // 3
+    #             if board[i][j] == '.':
+    #                 blanks.append((i, j))
+    #             else:
+    #                 rows[i].add(value)
+    #                 cols[j].add(value)
+    #                 blocks[bIndex].add(value)
+    #
+    #     # 将blanks一个个使用回溯法填入满足当前条件的数。
+    #     def backTrace(n):
+    #         # 遍历结束，此支线success。
+    #         if n == len(blanks):
+    #             return True
+    #         # 获取空缺的坐标
+    #         i, j = blanks[n]
+    #         bIndex = i // 3 * 3 + j // 3
+    #         # 获取(i, j)可以填入的数字rest
+    #         rest = nums - rows[i] - cols[j] - blocks[bIndex]
+    #         # rest为空时，表示当前空缺没办法填入数组，递归失败。
+    #         if not rest:
+    #             return False
+    #         # 逐个放入可填数字，并递归判断此支线的可行性。
+    #         for number in rest:
+    #             board[i][j] = number
+    #             rows[i].add(number)
+    #             cols[j].add(number)
+    #             blocks[bIndex].add(number)
+    #             # 递归进行验证下一可填数字。
+    #             if backTrace(n + 1):
+    #                 return True
+    #             rows[i].remove(number)
+    #             cols[j].remove(number)
+    #             blocks[bIndex].remove(number)
+    #     backTrace(0)
+    #
+    # board = [
+    #     ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+    #     ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+    #     [".", "9", "8", ".", ".", ".", ".", "6", "."],
+    #     ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+    #     ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+    #     ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+    #     [".", "6", ".", ".", ".", ".", "2", "8", "."],
+    #     [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+    #     [".", ".", ".", ".", "8", ".", ".", "7", "9"]
+    # ]
+    # solveSudoku(board)
+    # print(board)
--- 滑动窗口.py
+++ 滑动窗口.py
@@ -1,28 +1,63 @@
+import bisect
+from itertools import accumulate
 from typing import List
 
 if __name__ == '__main__':
+    # 可获得的最大值https://leetcode-cn.com/problems/maximum-points-you-can-obtain-from-cards/submissions/
+    # 由只能从首位拿取可以推得剩下的n - k 个数必然是连续最小值。
+    # 即找到最小连续数之和可找到最大值
+    def maxScore(cardPoints: List[int], k: int) -> int:
+        n = len(cardPoints)
+        # 滑窗大小 n - k
+        winSize = n - k
+        s =sum(cardPoints[:winSize])
+        mi = s
+        for start in range(winSize, n):
+            s += cardPoints[start] - cardPoints[start - winSize]
+            mi = min(mi, s)
+        return sum(cardPoints) - mi
 
+    cardPoints = [96,90,41,82,39,74,64,50,30]
+    k = 8
+    print(maxScore(cardPoints, k))
 
-    def characterReplacement(s: str, k: int) -> int:
-        n = len(s)
-        dic = {}
-        left = res = 0
-        for right in range(n):
-            # 统计字母出现次数
-            # dic.get(s[right], 0) 当dic中没有对应建，赋默认值0
-            dic[s[right]] = dic.get(s[right], 0) + 1
-            # 获取字典最值对应键 方法二： max(dic, key= dic.get)
-            maxLetter = max(dic, key=lambda x: dic[x])
-            # 将出现次数最大字母作为目标字幕，其余为待替换字母。
-            # 待替换字母数大于k时，左边界右缩,原左边界字母出现次数减一，更新最大出现次数字母
-            while right - left + 1 - dic[maxLetter] > k:
-                dic[s[left]] -= 1
-                left += 1
-                maxLetter = max(dic, key=lambda x: dic[x])
-            res = max(res, right - left + 1)
-        return res
-
-
-    s = "ABAA"
-    k = 0
-    print(characterReplacement(s, k))
+    # def equalSubstring(s: str, t: str, maxCost: int) -> int:
+    #     n = len(s)
+    #     # 计算s,t对应字母ascii码差值。并将结果累加(accumulate)
+    #     accDiff = [0] + list(accumulate(abs(ord(sc) - ord(tc)) for sc, tc in zip(s, t)))
+    #     maxLength = 0
+    #
+    #     for i in range(1, n + 1):
+    #         #
+    #         start = bisect.bisect_left(accDiff, accDiff[i] - maxCost)
+    #         maxLength = max(maxLength, i - start)
+    #
+    #     return maxLength
+    #
+    # s = "abcd"
+    # t = "bcdf"
+    # cost = 3
+    # print(equalSubstring(s, t, cost))
+    # def characterReplacement(s: str, k: int) -> int:
+    #     n = len(s)
+    #     dic = {}
+    #     left = res = 0
+    #     for right in range(n):
+    #         # 统计字母出现次数
+    #         # dic.get(s[right], 0) 当dic中没有对应建，赋默认值0
+    #         dic[s[right]] = dic.get(s[right], 0) + 1
+    #         # 获取字典最值对应键 方法二： max(dic, key= dic.get)
+    #         maxLetter = max(dic, key=lambda x: dic[x])
+    #         # 将出现次数最大字母作为目标字幕，其余为待替换字母。
+    #         # 待替换字母数大于k时，左边界右缩,原左边界字母出现次数减一，更新最大出现次数字母
+    #         while right - left + 1 - dic[maxLetter] > k:
+    #             dic[s[left]] -= 1
+    #             left += 1
+    #             maxLetter = max(dic, key=lambda x: dic[x])
+    #         res = max(res, right - left + 1)
+    #     return res
+    #
+    #
+    # s = "ABAA"
+    # k = 0
+    # print(characterReplacement(s, k))
