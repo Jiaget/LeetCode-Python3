0e4b1bd11d056336d5ccf1665ae13e3aca3416f4
ASUS <359453987@qq.com>
2021-03-10 16:30:12 +0800

    ~

---
+++ 堆栈.py
@@ -0,0 +1,26 @@
+if __name__ == '__main__':
+    def calculate(s: str) -> int:
+        res, num, sign = 0, 0, 1
+        stack = []
+        for c in s:
+            if c.isdigit():
+                num = 10 * num + int(c)
+            elif c == "+" or c == "-":
+                res += sign * num
+                num = 0
+                sign = 1 if c == "+" else -1
+            elif c == "(":
+                stack.append(res)
+                stack.append(sign)
+                res = 0
+                sign = 1
+            elif c == ")":
+                res += sign * num
+                num = 0
+                res *= stack.pop()
+                res += stack.pop()
+        res += sign * num
+        return res
+
+    s = "2147483647"
+    print(calculate(s))
