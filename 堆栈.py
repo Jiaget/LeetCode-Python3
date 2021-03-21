4c60c4afe8eefac9b0e5de0a8397ad8332e5e4cd
ASUS <359453987@qq.com>
2021-03-21 22:51:03 +0800

    逆波兰 一道竞赛题的性能优化

--- 堆栈.py
+++ 堆栈.py
@@ -1,24 +1,59 @@
 from typing import List
+import heapq
 
 if __name__ == '__main__':
-    def evalRPN(tokens: List[str]) -> int:
-        stack = []
-        for token in tokens:
-            if token not in '+-*/':
-                stack.append(int(token))
-            else:
-                b, a = stack.pop(), stack.pop()
-                if token == '+':
-                    stack.append(a + b)
-                elif token == '-':
-                    stack.append(a - b)
-                elif token == '*':
-                    stack.append(a * b)
-                elif token == '/':
-                    stack.append(int(a / b))
-        return stack.pop()
+    # 积压订单。
+    # 性能优化：1. 不要使用排序，使用最小堆heapq
+    #         2. 订单数为0要及时出栈，不要占据空间浪费性能。
+    def getNumberOfBacklogOrders(self, orders: List[List[int]]) -> int:
+        bqueue = []
+        squeue = []
+        for price, amount, typ in orders:
+            # 采购订单
+            if typ == 0:
+                while squeue and amount > 0 and squeue[0][0] <= price:
+                    temp = squeue[0][1] # 最小销售价格的订单笔数
+                    squeue[0][1] -= amount
+                    if squeue[0][1] <= 0:
+                        heappop(squeue)
+                    amount -= temp
+                if amount > 0: # 若采购单任然未完成
+                    heappush(bqueue, [-price, amount])
+            if typ == 1:
+                while bqueue and amount > 0 and abs(bqueue[0][0]) >= price:
+                    temp = bqueue[0][1]
+                    bqueue[0][1] -= amount
+                    if bqueue[0][1] <= 0:
+                        heappop(bqueue)
+                    amount -= temp
+                if amount > 0:
+                    heappush(squeue, [price, amount])
+        res = 0
 
-    print(evalRPN(["10","6","9","3","+","-11","*","/","*","17","+","5","+"]))
+        for buy in bqueue:
+            res += buy[1]
+        for sell in squeue:
+            res += sell[1]
+        return res % (10**9 + 7)
+
+    # def evalRPN(tokens: List[str]) -> int:
+    #     stack = []
+    #     for token in tokens:
+    #         if token not in '+-*/':
+    #             stack.append(int(token))
+    #         else:
+    #             b, a = stack.pop(), stack.pop()
+    #             if token == '+':
+    #                 stack.append(a + b)
+    #             elif token == '-':
+    #                 stack.append(a - b)
+    #             elif token == '*':
+    #                 stack.append(a * b)
+    #             elif token == '/':
+    #                 stack.append(int(a / b))
+    #     return stack.pop()
+    #
+    # print(evalRPN(["10","6","9","3","+","-11","*","/","*","17","+","5","+"]))
 
     # def isValidSerialization( preorder: str) -> bool:
     #     token = '*'
